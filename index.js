8b1d78bc2c82b0e304891ca95020ea8d3549e69d
MatiMoringa <110281834+MatiMoringa@users.noreply.github.com>
2022-08-23 23:02:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-function if{
-  // describe('index.js', function () {
-  describe('if', function () {
+describe('index.js', function () {
+  describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(if(199)).to.equal('This one is on me!');
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
     });
-}
 
-function ternaryCheckCity(){
-  // function addFive(someNumber) {
-  //Everything I want my function to do I put inside these curly braces
-  //In this example, let's say I want my function, addFive, to add 5 to
-  //any number I pass in (someNumber), but only IF the number is greater
-  //than zero:
-  let result
-  if (someNumber > 0) {
-    result = someNumber + 5;
-  }
-  //at the end, if I want my function to return something, I need to state it:
-  return result
-}
+    it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
 
-//once our function is declared, we can call addFive, passing in values 
-//as arguments:
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
 
-addFive(10);
-//=> 15
+  describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
 
-addFive(20);
-//=> 25
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });
 
-addFive(-5);
-//=> undefined
+  describe('switchOnCharmFromTip()', function () {
+    it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
 
-addFive(addFive(5));
-//=> 15!! In this case, the return value of addFive(5), 10, is passed in 
-//as the argument to the outer addFive, returning 15
-}
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
 
-function switch{
-  // function if {
-  //this is where we can use conditionals given our argument, someValue
-  //don't forget to return whatever the result is!
-}
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    });
+  });
